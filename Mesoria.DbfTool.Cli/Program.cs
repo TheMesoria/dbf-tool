a369fd06b277d1696577b58d3a622c217b4575ad
ormaniec <wojciech.ormaniec@gmail.com>
2021-04-19 20:03:41 +0200

    new: Add first preview of project layout

---
+++ Mesoria.DbfTool.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mesoria.DbfTool.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
